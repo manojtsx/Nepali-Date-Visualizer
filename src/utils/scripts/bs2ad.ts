d0ce01f832c9be274fe82d192f0321987fd896a4
Manoj Shrestha <shresthanewar678@gmail.com>
2025-07-29 22:18:44 +0545

    fix: fixed ts types

--- src/utils/scripts/bs2ad.ts
+++ src/utils/scripts/bs2ad.ts
@@ -2,21 +2,21 @@
 /* eslint-disable no-console */
 const NepaliDateClass = require('../NepaliDate').default;
 
-function pad(n) {
+function pad(n: number) {
   if (n < 10) {
     return `0${n}`;
   }
   return `${n}`;
 }
 
-function formatDate(d ) {
+function formatDate(d: Date) {
   return `${d.getFullYear()}-${pad(d.getMonth() + 1)}-${pad(d.getDate())}`;
 }
 
 try {
   const bs = process.argv[2] ? new NepaliDateClass(process.argv[2]) : new NepaliDateClass();
   console.log(formatDate(bs.getEnglishDate()));
-} catch (err) {
+} catch (err: any) {
   console.log(err.message);
   process.exit(1);
 }
